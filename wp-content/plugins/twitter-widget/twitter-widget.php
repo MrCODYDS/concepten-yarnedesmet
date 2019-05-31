2ded2dc8ccf25824ded19cf4938337024a157125
MrCODYDS <yarne.desmet@student.kdg.be>
2019-05-31 10:02:24 +0200

    Update twitter-widget

--- wp-content/plugins/twitter-widget/twitter-widget.php
+++ wp-content/plugins/twitter-widget/twitter-widget.php
@@ -29,30 +29,33 @@ class Twitter_Widget extends WP_Widget {
 	// Output the widget content on the front-end
 	public function widget( $args, $instance ) {
 		echo $args['before_widget'];
-		if ( ! empty( $instance['title'] ) ) {
+		/*if ( ! empty( $instance['title'] ) ) {
 			echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ) . $args['after_title'];
 		}
 		
-		echo $instance['link'];
+		echo $instance['link'];*/
+		?>
+		<blockquote class="twitter-tweet"><p lang="en" dir="ltr">Tomorrow is mystery! <a href="https://twitter.com/hashtag/Tomorrowland?src=hash&amp;ref_src=twsrc%5Etfw">#Tomorrowland</a> <a href="https://twitter.com/hashtag/AmicorumSpectaculum?src=hash&amp;ref_src=twsrc%5Etfw">#AmicorumSpectaculum</a> <a href="https://twitter.com/hashtag/goldenticket?src=hash&amp;ref_src=twsrc%5Etfw">#goldenticket</a> <a href="https://twitter.com/hashtag/artwork?src=hash&amp;ref_src=twsrc%5Etfw">#artwork</a> <a href="https://t.co/3pvg4728EH">pic.twitter.com/3pvg4728EH</a></p>&mdash; Yarne De Smet (@MrCODYDS) <a href="https://twitter.com/MrCODYDS/status/880899516370743298?ref_src=twsrc%5Etfw">June 30, 2017</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+		<?php
 		
 		echo $args['after_widget'];
 	}
 
 	// Output the option form field in admin Widgets screen
 	public function form( $instance ) {
-		?>
+		/*?>
 			<label >Enter Link:</label>
 			<input type="text" name="<?php echo $this->get_field_name('link'); ?>" value="<?php echo $instance["link"] ?>">
 
-		<?php
+		<?php*/
 	}
 
 	// Save options
 	public function update( $new_instance, $old_instance ) {
-		$instance = array();
+		/*$instance = array();
 		$instance['link'] = strip_tags($new_instance['link']);
 
-		return $instance;
+		return $instance;*/
 	}
 
 }
